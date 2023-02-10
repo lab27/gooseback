39fabfcd7e3631bf60fb39198ec46eeeb503bc9e
Nick Niles <nick@nickniles.com>
2023-02-10 11:24:36 -0800

    [feat] rays and settings config

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template lang="pug">
   main
     .tower-wrapper.cols-12
-      img(src="/img/tower.svg", alt="Gåsebäck Film Festival Hero Image")
+      //- img(src="/img/towerrays.svg", alt="Gåsebäck Film Festival Hero Image")
+      TowerRays
     .mobile-info-block.mt-12
       h1.text-center.mb-2 Gåsebäck
         br
@@ -48,6 +49,133 @@ export default {
 .tower-wrapper img {
   @apply m-0 lg:my-20;
   height: 70vh;
+}
+
+#red {
+  animation: redflash 5s linear infinite;
+}
+
+.ray1 {
+  animation: rayflash1 5s 0.1s linear infinite;
+}
+
+.ray2 {
+  animation: rayflash1 5s linear infinite;
+}
+
+.ray3 {
+  animation: rayflash3 5s linear infinite;
+}
+
+.ray4 {
+  animation: rayflash4 5s 0.2s linear infinite;
+}
+
+.ray5 {
+  animation: rayflash4 5s 0.25s linear infinite;
+}
+
+.ray6, .ray7 {
+  animation: rayflash6 5s 0.25s linear infinite;
+}
+
+.ray7 {
+  animation-delay: 300ms;
+}
+
+
+@keyframes redflash {
+  5% {
+    opacity: 1;
+  }
+
+  10% {
+    opacity: 1;
+  }
+
+  20% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 0;
+  }
+}
+
+@keyframes rayflash1 { 
+  0% { 
+    stroke-dasharray: 100 538;
+    stroke-dashoffset: 94;
+  }
+
+  10% {
+    stroke-dasharray: 50 538;
+    stroke-dashoffset: -538;
+    opacity: 1;
+  }
+
+  100% {
+    stroke-dasharray: 50 538;
+    stroke-dashoffset: -538;
+    opacity: 0;
+  }
+}
+
+@keyframes rayflash3 {
+  0% {
+    stroke-dasharray: 100 330;
+    stroke-dashoffset: 94;
+  }
+
+  8% {
+    stroke-dasharray: 30 330;
+    stroke-dashoffset: -330;
+    opacity: 1;
+  }
+
+  100% {
+    stroke-dasharray: 30 330;
+    stroke-dashoffset: -330;
+    opacity: 0;
+  }
+}
+
+@keyframes rayflash4 {
+  0% {
+    stroke-dasharray: 200 1261;
+    stroke-dashoffset: 94;
+  }
+
+  10% {
+    stroke-dasharray: 30 1261;
+    stroke-dashoffset: -1261;
+    opacity: 1;
+  }
+
+  100% {
+    stroke-dasharray: 30 1261;
+    stroke-dashoffset: -1261;
+    opacity: 0;
+  }
+}
+
+@keyframes rayflash6 {
+  0% {
+    stroke-dasharray: 200 1225;
+    stroke-dashoffset: 94;
+  }
+
+  10% {
+    stroke-dasharray: 30 1225;
+    stroke-dashoffset: -1225;
+    opacity: 1;
+  }
+
+  100% {
+    stroke-dasharray: 30 1225;
+    stroke-dashoffset: -1225;
+    opacity: 0;
+  }
 }
 </style>
 
