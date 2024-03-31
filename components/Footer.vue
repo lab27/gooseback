8760d2daa9000d83f80278139cd0685d5fec1a89
Nick Niles <nick@nickniles.com>
2024-03-30 18:46:41 -0700

    [feat] add space before tassu

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,6 +2,7 @@
   footer.site-footer
     Socials.mb-4
     p.copyright-info © 2023 Gåsebäck Film Festival
-    p Website made with love by
-      a(href="https://tassu.net" target="_blank").underlined-link Tassu
+    p Website made with love by&nbsp;
+      a(href="https://tassu.net" target="_blank").underlined-link
+        span Tassu
 </template>
