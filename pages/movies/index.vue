e90f4e59ad6c5b28d6b58b0835b3ba2cc04ccb5d
Nick Niles <nick@nickniles.com>
2024-04-29 07:13:42 -0700

    [feat] add movies content

--- pages/movies/index.vue
+++ pages/movies/index.vue
@@ -2,12 +2,12 @@
   main
     h1 The Movies
     //- nuxt-content.lead-text(:document="filmPage")
-    p Program to be announced mid-August 2024
-    br
-    a(href="https://filmfreeway.com/GasebackFilmFestival" target="_blank").submit-link
+    //- p Program to be announced mid-August 2024
+    //- br
+    //- a(href="https://filmfreeway.com/GasebackFilmFestival" target="_blank").submit-link
       span.email Submit A Film
       span.external &nbsp;&#8599;
-    //- ul.film-grid
+    ul.film-grid
       li(v-for="film in films")
         nuxt-link(:to="`/movies/${film.slug}`")
           .film-thumbnail-wrapper
@@ -15,7 +15,7 @@
               //- img(src="/img/arrow.svg")
               Arrow
             .thumbnail-gauze
-            nuxt-img(:src="$staticRemover(film.thumbnail)" :alt="film.title" :placeholder="[160, 90, 10]" format="webp" fit="cover" width="1600" height="900" sizes="sm:100vw md:50vw lg:400px xl:800px" preload).thumbnail-image
+            NuxtImg(:src="$staticRemover(film.thumbnail)" :alt="film.title" :placeholder="[160, 90, 10]" format="webp" fit="cover" width="1600" height="900" sizes="sm:100vw md:50vw lg:400px xl:800px" preload).thumbnail-image
           p.film-title {{ film.title }}
 </template>
 
