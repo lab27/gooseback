6bb22dc87c6c926002171d626cc733dc57526885
Nick Niles <nick@nickniles.com>
2024-04-29 07:22:21 -0700

    [feat] fix films page

--- pages/movies/index.vue
+++ pages/movies/index.vue
@@ -1,13 +1,7 @@
 <template lang="pug">
   main
-    h1 The Movies
-    //- nuxt-content.lead-text(:document="filmPage")
-    //- p Program to be announced mid-August 2024
-    //- br
-    //- a(href="https://filmfreeway.com/GasebackFilmFestival" target="_blank").submit-link
-      span.email Submit A Film
-      span.external &nbsp;&#8599;
-    ul.film-grid
+    h1 {{ filmPage.heading }}
+    ul.film-grid(v-if="filmPage.isAnnounced")
       li(v-for="film in films")
         nuxt-link(:to="`/movies/${film.slug}`")
           .film-thumbnail-wrapper
@@ -17,6 +11,9 @@
             .thumbnail-gauze
             NuxtImg(:src="$staticRemover(film.thumbnail)" :alt="film.title" :placeholder="[160, 90, 10]" format="webp" fit="cover" width="1600" height="900" sizes="sm:100vw md:50vw lg:400px xl:800px" preload).thumbnail-image
           p.film-title {{ film.title }}
+    section(v-else)
+      nuxt-content(:document="filmPage").lead-text
+
 </template>
 
 <script>
