4ef8aed72e9caed45f447cb98e50712d0e6911be
Nick Niles <nick@nickniles.com>
2024-07-29 10:20:27 +0200

    [feat] add FB link

--- components/Socials.vue
+++ components/Socials.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   ul.socials-grid
     li
-      a(href="#" target="_blank")
+      a(href="https://www.facebook.com/profile.php?id=61559622346848" target="_blank")
         img(src="/img/socials/facebook.svg", alt="Facebook")
     li
       a(href="https://twitter.com/gasebackff" target="_blank")
@@ -9,8 +9,16 @@
     li
       a(href="https://www.instagram.com/gasebackfilmfestival/" target="_blank")
         img(src="/img/socials/instagram.svg", alt="Instagram feed for Gåsebäck Film Festival")
-    li
+    //- li
       a(href="#" target="_blank")
         img(src="/img/socials/tt.svg", alt="Tik Tok")
     
 </template>
+
+<style>
+.socials-grid {
+  background-color: rgba(255, 255, 255, .5);
+  padding: .5rem 1rem;
+  border-radius: 1rem;
+}
+</style>
