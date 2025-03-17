ff31d28cfdefd92899f80e50658bf82975f690ef
Nick Niles <nick@nickniles.com>
2025-03-17 14:21:24 -0700

    [feat] add hels kitchen button apply

--- pages/helskitchen.vue
+++ pages/helskitchen.vue
@@ -2,6 +2,9 @@
   main
     h1 Hel's Kitchen
     p.lead-text Hel's Kitchen is our new independent film market. More info to follow!
+    br
+    a(href="https://docs.google.com/forms/d/e/1FAIpQLSd1Pe3QpoSFk9iqCBf-QeXC7OvPCrkwzSqfgPBV_Tkqg4cr2g/viewform?usp=header" target="_blank" ref="buttonRef").tickets-button
+      span.tickets-button__text Apply here&nbsp;&#8599;
 </template>
 
 <script setup lang="ts">
